dd62da154b1f0817f23851d34136dd5890878fa8
nik-mLb <148656910+nik-mLb@users.noreply.github.com>
2025-02-19 21:50:23 +0300

    Чистка файла index.js (#1)

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import Button from "./components/button/button.js";
-
-const b = new Button()
-
-const r = document.getElementById("root")
-r.innerHTML = b.render({"title":"press", "color":"green"})
