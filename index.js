3f8d4704acba7f669b2cd87a3595a8d1753fb42e
Kruglov Leonid <55053376+lenya1567@users.noreply.github.com>
2025-02-20 00:14:09 +0300

    Добавление модуля валидации (#4)

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 const dirname = path.dirname(new URL(import.meta.url).pathname);
 
 app.use('/src', express.static('src'));
-
-app.use('/public',express.static('public'));
+app.use('/public', express.static('public'));
+app.use('/modules', express.static('modules'));
 
 app.get('/', (req, res) => {
     const filePath = path.resolve(dirname, 'public', "index.html");
---
+++ modules/validation.js
@@ -0,0 +1,28 @@
+export const VALID_TYPES = {
+    EMAIL_VALID: 0,
+    TELEPHONE_VALID: 1,
+    PASSWORD_VALID: 2
+};
+
+/**
+ * Производит валидацию данных определённого типа.
+ * @param {VALID_TYPES} validationType Тип валидации (email, телефон или пароль)
+ * @param {String} data Данные для валидации
+ * @example
+ * // returns true
+ * validate(VALID_TYPES.EMAIL_VALID, "lepeshka@gmail.com")
+ * 
+ * // returns false
+ * validate(VALID_TYPES.PASSWORD_VALID, "1ldd")
+ * @returns {Boolean}
+ */
+export default function validate(validationType, data) {
+    switch (validationType) {
+        case VALID_TYPES.EMAIL_VALID:
+            return /^\w+(\.\w*)*@\w+(\.\w{2,})+$/g.exec(data) != null;
+        case VALID_TYPES.TELEPHONE_VALID:
+            return /^\+*[78]*\s*\(*\s*-*\d{,3}\)*\s*-*\d{3}\-*\d{2}\-*\d{2}$/g.exec(data) != null;
+        case VALID_TYPES.PASSWORD_VALID:
+            return /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/.exec(data) != null;
+    }
+}
