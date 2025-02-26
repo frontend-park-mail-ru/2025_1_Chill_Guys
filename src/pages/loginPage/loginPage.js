95c57810afbb458da1341fd83db7366b7682d935
Kruglov Leonid <55053376+lenya1567@users.noreply.github.com>
2025-02-27 01:26:07 +0300

    Вёрстка страницы входа (#14)

--- src/pages/loginPage/loginPage.js
+++ src/pages/loginPage/loginPage.js
@@ -1,5 +1,7 @@
 import BasePage from "../basePage.js";
 import Button from "../../../src/components/button/button.js";
+import Form from "../../../src/components/form/form.js";
+import { VALID_TYPES } from "../../../modules/validation.js";
 
 class LoginPage extends BasePage {
     constructor() {
@@ -9,12 +11,26 @@ class LoginPage extends BasePage {
     render(root, props) {
         root.innerHTML = super.render({});
         const mainElement = root.querySelector("main");
-        const buttonElement = new Button();
+        const formElement = mainElement.querySelector(".login_page__content__form");
 
-        mainElement.appendChild(buttonElement.render({
-            type: "success",
-            title: "Click me!",
-        }));
+        const loginForm = new Form([
+            {
+                type: "text",
+                id: "email",
+                name: "Почта",
+                validType: VALID_TYPES.EMAIL_VALID,
+                errorMessage: "Введите реальную почту"
+            },
+            {
+                type: "password",
+                id: "password",
+                name: "Пароль",
+                validType: VALID_TYPES.PASSWORD_VALID,
+                errorMessage: "Слишком простой пароль"
+            },
+        ], []);
+
+        formElement.appendChild(loginForm.render());
     }
 
     cleanUp() { }
