c17e429aacd6b25c539642b0dc1400c23aad6f1f
Mikhail <64345330+mishamarkov15@users.noreply.github.com>
2025-02-28 20:08:46 +0300

    Мега-модульная компонента кнопки (#23)
    * [feature] Добавлен файл с глобальными цветами. Добавлена компонента карточки товара
    
    * [feature] Исправил компоненту таким образом, что теперь иконку можно добавлять в любое место [top, left, right, bottom]. Или делать кнопку вообще без иконки.

--- src/components/button_with_icon/button_with_icon.js
+++ src/components/button_with_icon/button_with_icon.js
@@ -2,18 +2,42 @@
 
 import BaseComponent from "../baseComponent.js";
 
+export const ICON_POSITION = {
+    TOP: "button__orientation__top",
+    RIGHT: "button__orientation__right",
+    BOTTOM: "button__orientation__bottom",
+    LEFT: "button__orientation__left",
+}
+
 class ButtonWithIcon extends BaseComponent {
     constructor() {
         super("button_with_icon/button_with_icon");
     }
 
+    /**
+     *
+     * Кнопка с иконкой или без неё.
+     *
+     * @param props
+     *  - iconSrc: Путь до иконки для кнопки. Будет установлен в атрибут src.
+     *             Если пустой, то иконки не будет (будет просто текст).
+     *  - iconPosition: Расположение кнопки относительно текста [см. ICON_POSITION].
+     *  - iconAlt: Атрибут alt для иконки.
+     *  - title: Текст на самой кнопке
+     *  - isDisabled: Активна/Не активна.
+     *  - mainClass: Основной окрас кнопки.
+     *  - otherClasses: Строка со стилями, разделенными пробелами.
+     * @returns {*}
+     */
     render(props) {
         const element = super.render({
-            type: props.type,
-            title: props.title,
             isDisabled: props.disabled ? "disabled" : "",
+            mainClass: props.mainClass,
+            otherClasses: props.otherClasses,
+            title: props.title,
             iconAlt: props.iconAlt,
             iconSrc: props.iconSrc,
+            iconPosition: props.iconPosition || ICON_POSITION.LEFT,
         });
         element.addEventListener("click", props.onClick);
         return element;
---
+++ src/components/productCard/productCard.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import BaseComponent from "../baseComponent.js";
+
+class ProductCard extends BaseComponent {
+    constructor() {
+        super("productCard/productCard");
+    }
+}
+
+export default ProductCard;
