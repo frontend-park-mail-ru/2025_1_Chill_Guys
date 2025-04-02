fbf5d03b11614ec99bdb560040d144c7b0c6c00a
Mikhail Markov <mikhaiil@mmarkovv.ru>
2025-04-02 20:49:15 +0300

    [bugfix] Исправлены баги с иконками на кнопках. Добавлены именованные константы для размера кнопок, позиции иконки и типа кнопки

--- src/new_components/Button/Button.jsx
+++ src/new_components/Button/Button.jsx
@@ -1,11 +1,30 @@
 import Tarakan from "../../../modules/tarakan.js";
 import "./styles.scss";
 
+export const ICON_POSITION = {
+    TOP: "button__orientation__top",
+    RIGHT: "button__orientation__right",
+    BOTTOM: "button__orientation__bottom",
+    LEFT: "button__orientation__left",
+};
+
+export const BUTTON_VARIANT = {
+    TEXT: "text",
+    PRIMARY: "primary",
+};
+
+export const BUTTON_SIZE = {
+    XS: "xs",
+    S: "s",
+    M: "m",
+    L: "l",
+};
+
 class Button extends Tarakan.Component {
     render(props) {
-        const size = props.size ?? "l";
-        const variant = props.variant ?? "primary";
-        const iconPosition = props.iconPosition ?? "left";
+        const size = props.size ?? BUTTON_SIZE.L;
+        const variant = props.variant ?? BUTTON_VARIANT.PRIMARY;
+        const iconPosition = props.iconPosition ?? ICON_POSITION.LEFT;
         const otherClasses = props.className ?? "";
 
         return <button
@@ -14,7 +33,7 @@ class Button extends Tarakan.Component {
             onClick={(event) => props.onClick ? props.onClick(event) : {}}
         >
             {
-                props.iconSrc && <img alt={`${iconAlt}`} src={`${iconSrc}`} class="icon" />
+                props.iconSrc && <img alt={`${props.iconAlt}`} src={`${props.iconSrc}`} class="icon" />
             }
             <span>{props.title}</span>
         </button>
