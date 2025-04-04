87c2885891cba23b266e5969d4cb12fe60b05913
Mikhail Markov <mikhaiil@mmarkovv.ru>
2025-04-04 15:22:50 +0300

    [feature] Теперь хэдэр по-разному отображается для авторизованных пользователей и гостей

--- src/new_components/Header/Header.jsx
+++ src/new_components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import Tarakan from "../../../modules/tarakan.js";
+import ajax from "../../../modules/ajax.js";
 
 import Button, {BUTTON_VARIANT, ICON_POSITION} from "../Button/Button.jsx";
 import TextField from "../TextField/TextField.jsx";
 import {BUTTON_SIZE} from "../Button/Button.jsx";
 import {TEXTFIELD_TYPES} from "../TextField/TextField.jsx";
+import {SERVER_URL} from "../../settings.js";
 
 import "./styles.scss";
 
@@ -22,16 +24,20 @@ import HeaderCartHover from "../../shared/images/header-cart-ico-hover.svg"
 import HeaderProfile from "../../shared/images/header-profile-ico.svg";
 import HeaderProfileHover from "../../shared/images/header-profile-ico-hover.svg";
 
+import HeaderLogin from "../../shared/images/header-profile-enter-ico.svg";
+import HeaderLoginHover from "../../shared/images/header-profile-enter-ico-hover.svg";
+
 class Header extends Tarakan.Component {
     state = {
         ordersIcon: HeaderOrders,
         cartIcon: HeaderCart,
         savedIcon: HeaderSaved,
-        profileIcon: HeaderProfile,
+        profileIcon: this.isAuthorized() ? HeaderProfile : HeaderLogin,
+        authorized: this.isAuthorized(),
     }
 
     render(props, router) {
-        console.log(router);
+        console.log(`Authorized: ${this.state.authorized}`)
         return <header className={`header light flex column gap10px`}>
 
             <div className={`row flex main`}>
@@ -47,7 +53,9 @@ class Header extends Tarakan.Component {
                         title='Каталог'
                         iconSrc={`${CatalogButtonIcon}`}
                         iconAlt='Иконка каталога'
-                        onClick={() => {console.log('hello, world!');}}
+                        onClick={() => {
+                            console.log('hello, world!');
+                        }}
                     />
 
                     <div className={`tf`}>
@@ -61,30 +69,49 @@ class Header extends Tarakan.Component {
 
                 <div className={`icons-wrapper flex`}>
 
-                    <Button
-                        size={`${BUTTON_SIZE.L}`}
-                        iconPosition={`${ICON_POSITION.TOP}`}
-                        variant={`${BUTTON_VARIANT.TRANSPARENT}`}
-                        title='Заказы'
-                        iconSrc={`${this.state.ordersIcon}`}
-                        iconAlt='Иконка заказов'
-                        onMouseOver={() => {this.setState({ordersIcon: HeaderOrdersHover})}}
-                        onMouseLeave={() => {this.setState({ordersIcon: HeaderOrders})}}
-                        onClick={() => {console.log('Открываю заказы');}}
-                    />
+                    {
+                        this.state.authorized
+                        &&
+                        <Button
+                            size={`${BUTTON_SIZE.L}`}
+                            iconPosition={`${ICON_POSITION.TOP}`}
+                            variant={`${BUTTON_VARIANT.TRANSPARENT}`}
+                            title='Заказы'
+                            iconSrc={`${this.state.ordersIcon}`}
+                            iconAlt='Иконка заказов'
+                            onMouseOver={() => {
+                                this.setState({ordersIcon: HeaderOrdersHover})
+                            }}
+                            onMouseLeave={() => {
+                                this.setState({ordersIcon: HeaderOrders})
+                            }}
+                            onClick={() => {
+                                console.log('Открываю заказы');
+                            }}
+                        />
+                    }
 
-                    <Button
-                        size={`${BUTTON_SIZE.L}`}
-                        iconPosition={`${ICON_POSITION.TOP}`}
-                        variant={`${BUTTON_VARIANT.TRANSPARENT}`}
-                        title='Избранное'
-                        iconSrc={`${this.state.savedIcon}`}
-                        iconAlt='Иконка сердечко'
-                        onMouseOver={() => {this.setState({savedIcon: HeaderSavedHover})}}
-                        onMouseLeave={() => {this.setState({savedIcon: HeaderSaved})}}
-                        onClick={() => {console.log('Открываю избранное');}}
-                    />
-
+                    {
+                        this.state.authorized
+                        &&
+                        <Button
+                            size={`${BUTTON_SIZE.L}`}
+                            iconPosition={`${ICON_POSITION.TOP}`}
+                            variant={`${BUTTON_VARIANT.TRANSPARENT}`}
+                            title='Избранное'
+                            iconSrc={`${this.state.savedIcon}`}
+                            iconAlt='Иконка сердечко'
+                            onMouseOver={() => {
+                                this.setState({savedIcon: HeaderSavedHover})
+                            }}
+                            onMouseLeave={() => {
+                                this.setState({savedIcon: HeaderSaved})
+                            }}
+                            onClick={() => {
+                                console.log('Открываю избранное');
+                            }}
+                        />
+                    }
                     <Button
                         size={`${BUTTON_SIZE.L}`}
                         iconPosition={`${ICON_POSITION.TOP}`}
@@ -92,23 +119,46 @@ class Header extends Tarakan.Component {
                         title='Корзина'
                         iconSrc={`${this.state.cartIcon}`}
                         iconAlt='Иконка корзины'
-                        onMouseOver={() => {this.setState({cartIcon: HeaderCartHover})}}
-                        onMouseLeave={() => {this.setState({cartIcon: HeaderCart})}}
-                        onClick={() => {console.log('Открываю корзину');}}
+                        onMouseOver={() => {
+                            this.setState({cartIcon: HeaderCartHover})
+                        }}
+                        onMouseLeave={() => {
+                            this.setState({cartIcon: HeaderCart})
+                        }}
+                        onClick={() => {
+                            console.log('Открываю корзину');
+                        }}
                     />
 
                     <Button
                         size={`${BUTTON_SIZE.L}`}
                         iconPosition={`${ICON_POSITION.TOP}`}
                         variant={`${BUTTON_VARIANT.TRANSPARENT}`}
-                        title='Профиль'
+                        title={this.state.authorized ? 'Профиль' : 'Войти'}
                         iconSrc={`${this.state.profileIcon}`}
                         iconAlt='Иконка профиля'
-                        onMouseOver={() => {this.setState({profileIcon: HeaderProfileHover})}}
-                        onMouseLeave={() => {this.setState({profileIcon: HeaderProfile})}}
+                        onMouseOver={() => {
+                            console.log('profile mouse over');
+                            console.log(this.state.authorized);
+                            if (this.state.authorized) {
+                                this.setState({profileIcon: HeaderProfileHover});
+                            } else {
+                                this.setState({profileIcon: HeaderLoginHover});
+                            }
+                        }}
+                        onMouseLeave={() => {
+                            if (this.state.authorized) {
+                                this.setState({profileIcon: HeaderProfile});
+                            } else {
+                                this.setState({profileIcon: HeaderLogin});
+                            }
+                        }}
                         onClick={() => {
-                            console.log(`Router: ${router}`);
-                            router.navigateTo('/reg');
+                            if (this.state.authorized) {
+                                console.log('Скоро тут будет профиль');
+                            } else {
+                                router.navigateTo('/signin');
+                            }
                         }}
                     />
 
@@ -123,6 +173,11 @@ class Header extends Tarakan.Component {
             </div>
 
         </header>
+    }
+
+    isAuthorized() {
+        const res = ajax.get('api/users/me/');
+        return res.result?.status === 200;
     }
 }
 
