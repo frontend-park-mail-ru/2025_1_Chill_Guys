881f608c6c0c1d26d3b69ef05306efba12dda9ad
Mikhail Markov <mikhaiil@mmarkovv.ru>
2025-04-04 10:36:11 +0300

    [feature] Hader компонент переписан под jsx

--- src/new_components/Header/Header.jsx
+++ src/new_components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import Tarakan from "../../../modules/tarakan.js";
 
-import Button from "../Button/Button.jsx";
+import Button, {BUTTON_VARIANT, ICON_POSITION} from "../Button/Button.jsx";
 import TextField from "../TextField/TextField.jsx";
 import {BUTTON_SIZE} from "../Button/Button.jsx";
 import {TEXTFIELD_TYPES} from "../TextField/TextField.jsx";
@@ -10,11 +10,30 @@ import "./styles.scss";
 import LogoFull from "../../shared/images/LogoFull.svg";
 import CatalogButtonIcon from "../../shared/images/catalog-button-ico.svg";
 
-class Header extends Tarakan.Component {
-    render(props) {
-        return <header className={`flex-column header__light`}>
+import HeaderOrders from "../../shared/images/header-orders-ico.svg";
+import HeaderOrdersHover from "../../shared/images/header-orders-ico-hover.svg";
 
-            <div className={`header-row main-row`}>
+import HeaderSaved from "../../shared/images/header-saved-ico.svg";
+import HeaderSavedHover from "../../shared/images/header-saved-ico-hover.svg";
+
+import HeaderCart from "../../shared/images/header-cart-ico.svg";
+import HeaderCartHover from "../../shared/images/header-cart-ico-hover.svg"
+
+import HeaderProfile from "../../shared/images/header-profile-ico.svg";
+import HeaderProfileHover from "../../shared/images/header-profile-ico-hover.svg";
+
+class Header extends Tarakan.Component {
+    state = {
+        ordersIcon: HeaderOrders,
+        cartIcon: HeaderCart,
+        savedIcon: HeaderSaved,
+        profileIcon: HeaderProfile,
+    }
+
+    render(props) {
+        return <header className={`header light flex column gap10px`}>
+
+            <div className={`row flex main`}>
                 <img
                     className={`logo`}
                     alt={`Логотип маркетплейса Bazaar`}
@@ -34,16 +53,66 @@ class Header extends Tarakan.Component {
                         <TextField
                             type={`${TEXTFIELD_TYPES.SEARCH}`}
                             title={`Ищите что угодно на Bazaar`}
-                            otherClasses={`tf__type__input__width__400`}
+                            className={`width px400`}
                         />
                     </div>
                 </div>
 
-                <div className={`icons-wrapper flex`}></div>
+                <div className={`icons-wrapper flex`}>
+
+                    <Button
+                        size={`${BUTTON_SIZE.L}`}
+                        iconPosition={`${ICON_POSITION.TOP}`}
+                        variant={`${BUTTON_VARIANT.TRANSPARENT}`}
+                        title={`Заказы`}
+                        iconSrc={`${this.state.ordersIcon}`}
+                        iconAlt={`Иконка заказов`}
+                        onMouseOver={() => {this.setState({ordersIcon: HeaderOrdersHover})}}
+                        onMouseLeave={() => {this.setState({ordersIcon: HeaderOrders})}}
+                        onClick={() => {console.log('Открываю заказы');}}
+                    />
+
+                    <Button
+                        size={`${BUTTON_SIZE.L}`}
+                        iconPosition={`${ICON_POSITION.TOP}`}
+                        variant={`${BUTTON_VARIANT.TRANSPARENT}`}
+                        title={`Избранное`}
+                        iconSrc={`${this.state.savedIcon}`}
+                        iconAlt={`Иконка сердечко`}
+                        onMouseOver={() => {this.setState({savedIcon: HeaderSavedHover})}}
+                        onMouseLeave={() => {this.setState({savedIcon: HeaderSaved})}}
+                        onClick={() => {console.log('Открываю избранное');}}
+                    />
+
+                    <Button
+                        size={`${BUTTON_SIZE.L}`}
+                        iconPosition={`${ICON_POSITION.TOP}`}
+                        variant={`${BUTTON_VARIANT.TRANSPARENT}`}
+                        title={`Корзина`}
+                        iconSrc={`${this.state.cartIcon}`}
+                        iconAlt={`Иконка корзины`}
+                        onMouseOver={() => {this.setState({cartIcon: HeaderCartHover})}}
+                        onMouseLeave={() => {this.setState({cartIcon: HeaderCart})}}
+                        onClick={() => {console.log('Открываю корзину');}}
+                    />
+
+                    <Button
+                        size={`${BUTTON_SIZE.L}`}
+                        iconPosition={`${ICON_POSITION.TOP}`}
+                        variant={`${BUTTON_VARIANT.TRANSPARENT}`}
+                        title={`Профиль`}
+                        iconSrc={`${this.state.profileIcon}`}
+                        iconAlt={`Иконка профиля`}
+                        onMouseOver={() => {this.setState({profileIcon: HeaderProfileHover})}}
+                        onMouseLeave={() => {this.setState({profileIcon: HeaderProfile})}}
+                        onClick={() => {console.log('Открываю профиль');}}
+                    />
+
+                </div>
 
             </div>
 
-            <div className={`header-row secondary-row`}>
+            <div className={`row flex secondary`}>
                 <div className={`address-wrapper`}>
                     Улица Космонавтов 7
                 </div>
