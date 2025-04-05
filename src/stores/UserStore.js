f58fefad707b5572ff9c4e938814daa481f15414
Kruglov Leonid <lenya@leshak.ru>
2025-04-06 02:45:24 +0300

    fix

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -8,32 +8,33 @@ const initAction = (store) => store.sendAction("me");
 const UserStore = new Store(initValue, initAction);
 
 UserStore.addAction("me", async (store) => {
-    const res = await ajax.get("api/users/me", { origin: SERVER_URL });
-    if (!res.error && res.result.ok) {
-        const userData = await res.result.json();
-        store.sendAction("login", { login: true, ...userData });
-        return false;
-    }
-    return res.error || res.result.status;
+  const res = await ajax.get("api/users/me", { origin: SERVER_URL });
+  if (!res.error && res.result.ok) {
+    const userData = await res.result.json();
+    store.sendAction("login", { login: true, ...userData });
+    return false;
+  }
+  return res.error || res.result.status;
 });
 
 UserStore.addAction("signin", async (store, data) => {
-    const res = await ajax.post("api/auth/login", data, { origin: SERVER_URL, });
-    if (!res.error && res.result.ok) {
-        store.sendAction("me");
-        return false;
-    }
-    return res.error || res.result.status;
+  const res = await ajax.post("api/auth/login", data, { origin: SERVER_URL });
+  if (!res.error && res.result.ok) {
+    store.sendAction("me");
+    return false;
+  }
+  return res.error || res.result.status;
 });
 
-
 UserStore.addAction("signup", async (store, data) => {
-    const res = await ajax.post("api/auth/register", data, { origin: SERVER_URL, });
-    if (!res.error && res.result.ok) {
-        store.sendAction("me");
-        return false;
-    }
-    return res.error || res.result.status;
+  const res = await ajax.post("api/auth/register", data, {
+    origin: SERVER_URL,
+  });
+  if (!res.error && res.result.ok) {
+    store.sendAction("me");
+    return false;
+  }
+  return res.error || res.result.status;
 });
 
 export default UserStore;
