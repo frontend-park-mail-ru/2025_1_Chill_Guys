3d5934b5db1ef18b51e916b9b53643449361556f
Mikhail Markov <mikhaiil@mmarkovv.ru>
2025-04-06 08:09:26 +0300

    [feature] Добавил загловок и заготовку под список карточек.

--- src/new_pages/IndexPage/IndexPage.jsx
+++ src/new_pages/IndexPage/IndexPage.jsx
@@ -3,11 +3,29 @@ import Header from "../../new_components/Header/Header.jsx";
 import Footer from "../../new_components/Footer/Footer.jsx";
 
 class IndexPage extends Tarakan.Component {
+
+    state = {}
+
     render(props, router) {
+
         return <div className={`container`}>
             <Header/>
 
-            <button onClick={() => router.navigateTo("/signin")}>Click</button>
+            <main className={`index-page`}>
+                <h1 className={`h-reset main-h1`}>Весенние хиты</h1>
+                <div className={`cards-container`}>
+                    {
+                        () => {
+                            const response = ajax.get('api/products/');
+                            if (response?.result.code === 200) {
+                                return <div>hello!</div>
+                            } else {
+                                return <div>error</div>
+                            }
+                        }
+                    }
+                </div>
+            </main>
 
             <Footer/>
          </div>
