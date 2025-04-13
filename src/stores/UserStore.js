48d0ea241b8f4cc580908d54d839728a633ab027
Kruglov Leonid <lenya@leshak.ru>
2025-04-13 03:43:46 +0300

    fix

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -9,29 +9,29 @@ const initAction = (store) => store.sendAction("me");
 const UserStore = new Store(initValue, initAction);
 
 UserStore.addAction("me", async (store) => {
-	const res = await getMe();
-	if (res.code === AJAXErrors.NoError) {
-		store.sendAction("login", { login: true, ...res.data });
-	} else {
-		store.sendAction("login", { login: false });
-	}
-	return res.code;
+  const res = await getMe();
+  if (res.code === AJAXErrors.NoError) {
+    store.sendAction("login", { login: true, ...res.data });
+  } else {
+    store.sendAction("login", { login: false });
+  }
+  return res.code;
 });
 
 UserStore.addAction("signin", async (store, data) => {
-	const code = await signIn(data.email, data.password);
-	if (code === AJAXErrors.NoError) {
-		store.sendAction("me");
-	}
-	return code;
+  const code = await signIn(data.email, data.password);
+  if (code === AJAXErrors.NoError) {
+    store.sendAction("me");
+  }
+  return code;
 });
 
 UserStore.addAction("signup", async (store, data) => {
-	const code = await signUp(data.name, data.surname, data.email, data.password);
-	if (code === AJAXErrors.NoError) {
-		store.sendAction("me");
-	}
-	return code;
+  const code = await signUp(data.name, data.surname, data.email, data.password);
+  if (code === AJAXErrors.NoError) {
+    store.sendAction("me");
+  }
+  return code;
 });
 
 export default UserStore;
